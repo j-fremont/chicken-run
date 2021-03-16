a3108adedc93bb395b65adf85407ba80b14359dd
j-fremont <j.fremont@sfr.fr>
2021-03-16 11:18:20 +0000

    Services supplementaires

--- chicken-app/flask-app.py
+++ chicken-app/flask-app.py
@@ -6,6 +6,7 @@ from flask_jsonpify import jsonify
 from datetime import datetime
 from time import sleep
 from picamera import PiCamera
+from os import listdir, remove
 
 app = Flask(__name__)
 
@@ -17,23 +18,26 @@ led = GPIO.PWM(3, 200);
 
 camera = PiCamera();
 
-@app.route("/picture/new", methods = ['GET'])
+@app.route("/picture/new", methods = ['POST'])
 def picture_new():
     now = datetime.now()
     dt_string = now.strftime("/%Y%m%d_%Hh%Mm%Ss")
     light_and_picture(dt_string)
-    return jsonify({'result':'ok'})
+    return jsonify({'result':'success'})
 
-#@app.route("/picture/get", methods = ['GET'])
-#def picture_get():
+@app.route("/picture/get/<file>", methods = ['GET'])
+def picture_get():
+    return send_file('/home/pi/pictures/' + file, mimetype='image/jpeg')
 
+@app.route("/picture/list", methods = ['POST'])
+def picture_list():
+    files = listdir('/home/pi/pictures')
+    return jsonify({'files':files})
 
-#@app.route("/picture/list", methods = ['POST'])
-#def picture_list():
-
-
-#@app.route("/picture/remove", methods = ['POST'])
-#def picture_remove():
+@app.route("/picture/remove/<file>", methods = ['POST'])
+def picture_remove():
+    remove('/home/pi/pictures/' + file)
+    return jsonify({'result':'success'})
 
 def light_and_picture(date_time):
     led.start(0);
