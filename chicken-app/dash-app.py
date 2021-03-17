52cb37d57b760c53eba1a2b3a50f950d99234f47
j-fremont <j.fremont@sfr.fr>
2021-03-17 11:00:52 +0100

    chicken-app image

--- chicken-app/dash-app.py
+++ chicken-app/dash-app.py
@@ -3,6 +3,8 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 
+from dash.dependencies import Input, Output
+
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SOLAR])
 
 app.layout = html.Div([
@@ -10,12 +12,12 @@ app.layout = html.Div([
         dbc.Col([
             dbc.Form([
                 dbc.FormGroup([
-                    dbc.Button("Prendre une nouvelle photo", color="primary", className="mr-1"),
+                    dbc.Button("Prendre une nouvelle photo", id="take_picture", color="primary", className="mr-1"),
                 ]),
                 dbc.FormGroup([
                     dbc.Label("Liste des photos", html_for="dropdown"),
                     dcc.Dropdown(
-                        id="dropdown",
+                        id="file_list",
                         options=[
                             {"label": "Option 1", "value": 1},
                             {"label": "Option 2", "value": 2},
@@ -33,9 +35,9 @@ app.layout = html.Div([
         dbc.Col([
 dbc.Card(
     [
-        dbc.CardImg(src="/static/images/placeholder286x180.png", top=True),
+        dbc.CardImg(id="card_image", top=True),
         dbc.CardBody(
-            html.P("This card has an image at the top", className="card-text")
+            html.P("This card has an image at the top", id="card_label", className="card-text")
         ),
     ],
     style={"width": "18rem"},
@@ -49,6 +51,18 @@ dbc.Card(
     "left": "25px"
     }
 )
+
+@app.callback(
+    Output("card_image", "src"), [Input("take_picture", "n_clicks"),]
+)
+def on_take_picture(n):
+    return app.get_asset_url('image.jpg')
+
+#@app.callback(
+#    [Input(component_id='file_list', component_property='value'),]
+#)
+#def update_file(file):
+#    print file
 
 if __name__ == '__main__':
     app.run_server(debug=True)
