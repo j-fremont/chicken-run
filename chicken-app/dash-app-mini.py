7be1b91145a16a59783b16db87273b830d0fc816
JEROME FREMONT <j75550@dsp0939929.atlas.edf.fr>
2022-04-26 15:47:52 +0200

    Test chicken-app

--- chicken-app/dash-app-mini.py
+++ chicken-app/dash-app-mini.py
@@ -4,6 +4,7 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 import requests
 import json
+import wget
 
 from dash.dependencies import Input, Output, State
 
@@ -30,13 +31,21 @@ app.layout = html.Div([
 
 @app.callback(
     Output("card_image", "src"),
-    Input("dummy", "children")
+    [Input("dummy", "children")]
 )
 def on_init(n):
-    response = requests.get('http://localhost:5000/picture/get/image.jpg', allow_redirects=True)
-    print(response.content)
-    return response.content
+    #wget.download('http://127.0.0.1:5000/picture/get/1648020012141.jpeg', out='/home/j75550/test.jpeg')
+    response = requests.get('http://localhost:5000/picture/get/1648020012141.jpeg', headers={'User-Agent': 'Mozilla/5.0'})
+    print(response.url)
+    print(response.status_code)
+    #if response.status_code == 200:
+    #    with open('/tmp/test.jpeg', 'wb') as f:
+    #        f.write(response.content)
+
+    #response = requests.get(, allow_redirects=True)
+    #open('test.jpeg', 'wb').write(response.content)
+    #print(response.content)
+    return True
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-    
--- chicken-app/flask-app.py
+++ chicken-app/flask-app.py
@@ -1,6 +1,4 @@
-import RPi.GPIO as GPIO
 import sys
-import mycamera
 
 from flask import Flask, request, json, send_file
 from flask_jsonpify import jsonify
@@ -9,16 +7,10 @@ from os import listdir, remove
 
 app = Flask(__name__)
 
-@app.route("/picture/new", methods = ['GET'])
-def picture_new():
-    now = datetime.now()
-    dt_string = now.strftime("%Y%m%d_%Hh%Mm%Ss")
-    mycamera.light_and_picture(dt_string)
-    return send_file('/home/pi/pictures/image_' + dt_string + '.jpg', mimetype='image/jpeg')
-
 @app.route("/picture/get/<file>", methods = ['GET'])
 def picture_get(file):
-    return send_file('/home/pi/Pictures/' + file, mimetype='image/jpeg')
+    print(file)
+    return send_file('/home/j75550/' + file, mimetype='image/jpeg')
 
 @app.route("/picture/list", methods = ['POST'])
 def picture_list():
@@ -32,4 +24,3 @@ def picture_remove(file):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
